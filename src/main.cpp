b87de543da1ccf33d917991482c94c474327e0b1
Martino Lai <140235799+martinolai@users.noreply.github.com>
2025-08-18 17:09:09 +0200

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,1 +1,14 @@
+#include <iostream>
+#include "Process.h"
 
+using namespace std;
+
+int main() {
+    Process p1(1, 5, 0);  // pid=1, burst=5, arrival=0
+    Process p2(2, 3, 2);  // pid=2, burst=3, arrival=2
+
+    cout << p1.toString() << endl;
+    cout << p2.toString() << endl;
+
+    return 0;
+}
